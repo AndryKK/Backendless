4a06681545fe0154055a661e38e126c1c71bfd7d
AndryKK <korbutiaktso@gmail.com>
2022-05-03 15:41:51 +0300

    first setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">
+      <h1 className="App-logo">
        Backendless
       </h1>
     </div>
